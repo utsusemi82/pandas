437a389661047781e5d9b6ccb570bd589406302b
jay <91657674+utsusemi82@users.noreply.github.com>
2025-02-23 16:24:26 +0000

    commit

---
+++ Create a DataFrame from a List.py
@@ -0,0 +1,7 @@
+#Create a DataFrame from a List
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    column_names = ["student_id", "age"]
+    result_dataframe = pd.DataFrame(student_data, columns=column_names)
+    return result_dataframe
